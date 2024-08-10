734933ba4694df4c3fba99a1a96d3e8b0e4949db
Tristan <trispebray@gmail.com>
2024-08-10 22:57:59 +0200

    implement ball friction and spin

--- src/physics/Scene.tsx
+++ src/physics/Scene.tsx
@@ -274,17 +274,33 @@ class PhysicsState {
     }
   }
 
-  applyForces(timeStep: number) {
-    for (let body of this.bodies) {
-      const frictionCoefficient = 1500
+  applyFriction(body: Body) {
+      const frictionCoefficient = 9000
       const frictionVector = body.velocity.Mult(-1)
       frictionVector.Normalize()
-      const friction  = frictionVector.Mult(frictionCoefficient)
-
+      const friction = frictionVector.Mult(frictionCoefficient)
       body.force = Vector2.Add(body.force, friction)
-      body.force = Vector2.Add(body.force, body.spin)
+  }
+
+  applySpin(body: Body) {
+      const spinCoefficient = 1000
+      const spin = body.spin.Mult(spinCoefficient)
+      body.force = Vector2.Add(body.force, spin)
+      body.spin = body.spin.Mult(0.98)
+  }
+
+  applyForces(timeStep: number) {
+    for (let body of this.bodies) {
+      this.applyFriction(body)
+      this.applySpin(body)
+
       const acceleration = body.force.Mult(body.inv_mass);
       body.velocity = Vector2.Add(body.velocity, acceleration.Mult(timeStep))
+
+      if (body.velocity.MagnitudeSquared() < 1) {
+        body.velocity = Vector2.ZERO
+      }
+
       body.transform.position = Vector2.Add(body.transform.position, body.velocity.Mult(timeStep))
       body.force = new Vector2({x: 0, y: 0})
     }
@@ -297,41 +313,46 @@ class PhysicsState {
   }
 }
 
-const createBall = (x: number, y: number, speed: number = 0, direction: Vector2 = new Vector2({x:0, y: 0}), spin?: Vector2) => {
+const createBall = (x: number, y: number, color: string, speed: number = 0, direction: Vector2 = new Vector2({x:0, y: 0}), spin?: Vector2) => {
     const velocity = direction.Mult(speed)
-    return new Body({spin: spin, mass: 100, restitution: 0.85, velocity: velocity, shape: new Circle({transform: new Transform({position: new Vector2({x: x, y: y}), rotation: 0}), radius: 10})})
+    return new Body({spin: spin, mass: 100, restitution: 0.95, velocity: velocity, shape: new Circle({transform: new Transform({position: new Vector2({x: x, y: y}), rotation: 0}), radius: 10, color: color})})
 }
 
 
 const Scene: Component<SceneProps> = (props: SceneProps) => {
   const {width, height} = props
-  const top = new Body({mass: 0, restitution: 0.8, velocity: new Vector2({x: 0, y: 0}), shape: new AABB({transform: new Transform({position: new Vector2({x: 200, y: 30}),rotation: 0}), width: 340, height: 30})})
-  const bottom = new Body({mass: 0, restitution: 0.8, velocity: new Vector2({x: 0, y: 0}), shape: new AABB({transform: new Transform({position: new Vector2({x: 200, y: 670}),rotation: 0}), width: 340, height: 30})})
-  const left = new Body({mass: 0, restitution: 0.8, velocity: new Vector2({x: 0, y: 0}), shape: new AABB({transform: new Transform({position: new Vector2({x: 30, y: 350}),rotation: 0}), width: 30, height: 640})})
-  const right = new Body({mass: 0, restitution: 0.8, velocity: new Vector2({x: 0, y: 0}), shape: new AABB({transform: new Transform({position: new Vector2({x: 370, y: 350}),rotation: 0}), width: 30, height: 640})})
+  const top = new Body({restitution: 0.8, shape: new AABB({transform: new Transform({position: new Vector2({x: 200, y: 30}),rotation: 0}), width: 340, height: 50})})
+  const bottom = new Body({restitution: 0.8, shape: new AABB({transform: new Transform({position: new Vector2({x: 200, y: 670}),rotation: 0}), width: 340, height: 50})})
+  const left = new Body({restitution: 0.8, shape: new AABB({transform: new Transform({position: new Vector2({x: 30, y: 350}),rotation: 0}), width: 30, height: 640})})
+  const right = new Body({restitution: 0.8, shape: new AABB({transform: new Transform({position: new Vector2({x: 370, y: 350}),rotation: 0}), width: 30, height: 640})})
   const balls = [
-    createBall(200, 200, 800, new Vector2({x: 0, y: 1})),
+    createBall(100, 650, "white", 1550, new Vector2({x: 0.05, y: -1}), new Vector2({x: 0, y: 300})),
 
-    createBall(200, 480),
+    createBall(200, 480, "red"),
 
-    createBall(190, 500),
-    createBall(210, 500),
+    createBall(190, 499, "yellow"),
+    createBall(210, 499, "red"),
 
-    createBall(180, 520),
-    createBall(200, 520),
-    createBall(220, 520),
+    createBall(180, 518, "red"),
+    createBall(200, 518, "black"),
+    createBall(220, 518, "yellow"),
 
-    createBall(170, 540),
-    createBall(190, 540),
-    createBall(210, 540),
-    createBall(230, 540),
+    createBall(170, 537, "yellow"),
+    createBall(190, 537, "red"),
+    createBall(210, 537, "yellow"),
+    createBall(230, 537, "red"),
+
+    createBall(160, 556, "red"),
+    createBall(180, 556, "yellow"),
+    createBall(200, 556, "red"),
+    createBall(220, 556, "yellow"),
+    createBall(240, 556, "yellow"),
   ]
   const objects = [
     top,
     bottom,
     left,
     right,
-    ...balls
   ]
   const state = new PhysicsState({bodies: objects})
   let canvas: HTMLCanvasElement | undefined;
